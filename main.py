a8761ef29487e886abd1d42f4cd243e65a7cffe7
Jiafei_Fan <fjf@bu.edu>
2025-01-12 22:37:02 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from models import *
+from tortoise import models
+from authentication import get_password_hash
+
+# signals
+from tortoise.signals import  post_save
+from typing import List, Optional, Type
+from tortoise import BaseDBAsyncClient
+
+app = FastAPI()
+
+
+# process signals here
+@post_save(User)
+async def create_business(
+        sender: "Type[User]",
+        instance: User,
+        created: bool,
+        using_db: "Optional[BaseDBAsyncClient]",
+        update_fields: List[str]) -> None:
+    if created:
+        business_obj = await Business.create(
+            business_name=instance.username, owner=instance)
+        await business_pydantic.from_tortoise_orm(business_obj)
+        # send email functionality
+
+
+@app.post('/registration')
+async def user_registration(user: user_pydanticIn):
+    user_info = user.dict(exclude_unset=True)
+    user_info['password'] = get_password_hash(user_info['password'])
+    user_obj = await User.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_obj)
+
+    return {"status": "ok",
+            "data":
+                f"Hello {new_user.username} thanks for choosing our services. Please check your email inbox and click on the link to confirm your registration."}
+@app.get("/")
+def index():
+    return {"Message": "Hello World"}
+
+register_tortoise(
+    app,
+    db_url="sqlite://database.sqlite3",
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
